adadbea2bdb542558235ace3bddedb54c696181c
anmihovil <anmihovil@gmail.com>
2019-06-03 11:54:15 +0200

    first piggame commit

---
+++ app.js
@@ -0,0 +1,119 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, roundScore, activePlayer, prevDice, winningScore, isGamePlaying;
+
+init();
+
+document.querySelector(".dice").style.display = "none";
+document.querySelector(".btn-hold").style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  if (isGamePlaying) {
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    console.log(prevDice + " " + dice);
+
+    // 2. Display the Result
+    var diceDOM = document.querySelector(".dice");
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-" + dice + ".png";
+
+    // 3. Update the round score IF the rolled number is NOT 1
+    if (dice !== 1) {
+      // Add score
+      roundScore += dice;
+      aggScore = scores[activePlayer] + roundScore;
+
+      sixChecker(prevDice, dice);
+      displayResults(aggScore);
+
+      document.querySelector(
+        "#current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      // Add score from current round to averall score before player swap
+      scores[activePlayer] += roundScore;
+      document.querySelector("#score-" + activePlayer).textContent =
+        scores[activePlayer];
+
+      // Swap player
+      swapPlayer();
+    }
+    prevDice = dice;
+  }
+});
+
+document.querySelector(".btn-new").addEventListener("click", init);
+
+function displayResults(aggScore) {
+  if (aggScore >= winningScore) {
+    document.querySelector(".dice").style.display = "none";
+    document.querySelector("#score-" + activePlayer).textContent = aggScore;
+    document.querySelector("#name-" + activePlayer).textContent = "WINNER!";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.remove("active");
+    isGamePlaying = false;
+  }
+}
+
+function init() {
+  scores = [0, 0];
+  activePlayer = 0;
+  roundScore = 0;
+  prevDice = 0;
+  winningScore = 100;
+  isGamePlaying = true;
+
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  document.getElementById("name-0").textContent = "PLAYER 1";
+  document.getElementById("name-1").textContent = "PLAYER 2";
+
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+
+  document.querySelector(".player-0-panel").classList.add("active");
+  document.querySelector(".dice").style.display = "none";
+}
+
+function swapPlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+
+  document.querySelector(".dice").style.display = "none";
+}
+
+function sixChecker(prevDice, dice) {
+  if (prevDice === 6 && prevDice === dice) {
+    scores[activePlayer] = 0;
+    roundScore = 0;
+    document.querySelector("#score-" + activePlayer).textContent = "0";
+    prevDice = 0;
+    swapPlayer();
+  }
+}
